e1ab097eb7ede638c264ba96f1466b15ab9a8292
Alina <78365479+1amLina@users.noreply.github.com>
2025-09-06 23:01:45 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# users - это список пользователей, где каждый пользователь - это словарь.
+# Нужно вернуть список имен пользователей, которым больше 18 лет.
+
+def get_adult_users_names(users_data):
+    
+    names_list = []
+    
+    for user in users_data:
+        # Проверяем, есть ли ключ 'age' и больше ли он 18
+        if 'age' in user and user['age'] > 18:
+            names_list.append(user['name'])
+            
+    return names_list
+
+# Пример данных для проверки
+all_users = [
+    {'name': 'Иван', 'age': 25},
+    {'name': 'Мария', 'age': 17},
+    {'name': 'Петр', 'age': 32},
+    {'name': 'Анна'} # У этого пользователя нет ключа 'age'
+]
+
+adult_names = get_adult_users_names(all_users)
+print(f"Имена совершеннолетних пользователей: {adult_names}")
